6793b05076e68d929244b183204dc9652edb5a14
Jonas Ekskog <jonas.ekskog@gmail.com>
2024-06-01 01:51:00 +0200

    Create bitmap when stitches or brushes are updated.

--- src/components/canvas/canvas.tsx
+++ src/components/canvas/canvas.tsx
@@ -5,30 +5,19 @@ import { useBrushState, useStitchState } from '@store';
 import React, { FC, RefCallback } from 'react';
 import { Layer, Stage } from 'react-konva';
 import { styled } from 'styled-components';
-import { useCanvasControls } from './hooks';
 import { DRAFT_LAYER } from './constants';
+import { useCanvasControls } from './hooks';
 
 const CanvasContainer = styled.div`
   width: 100%;
   height: 100%;
 `;
 
-const renderCells = (
-  stitches: { [key: string]: Stitch },
-  brushes: { [key: string]: Brush }
-) => {
+const renderCells = (stitches: { [key: string]: Stitch }, brushes: { [key: string]: Brush }) => {
   const result: React.ReactElement[] = [];
   for (const stitch of Object.values(stitches)) {
     const brush = brushes[stitch.brush];
-    result.push(
-      <Cell
-        key={stitch.id}
-        x={stitch.x}
-        y={stitch.y}
-        shape={brush.shape}
-        color={brush.color}
-      />
-    );
+    result.push(<Cell key={stitch.id} x={stitch.x} y={stitch.y} shape={brush.shape} color={brush.color} />);
   }
   return result;
 };
@@ -42,20 +31,11 @@ const Canvas: FC<CanvasProps> = ({ container, size }) => {
   const { scale, center, handlers } = useCanvasControls(size);
   const brushes = useBrushState();
   const stitches = useStitchState();
-  const cells = React.useMemo(
-    () => renderCells(stitches.byId, brushes.byId),
-    [stitches.byId, brushes]
-  );
+  const cells = React.useMemo(() => renderCells(stitches.byId, brushes.byId), [stitches.byId, brushes]);
 
   return (
     <CanvasContainer ref={container} className="bg-light">
-      <Stage
-        {...size}
-        scale={scale}
-        x={0.5 * size.width - center.x * scale.x}
-        y={0.5 * size.height - center.y * scale.y}
-        {...handlers}
-      >
+      <Stage {...size} scale={scale} x={0.5 * size.width - center.x * scale.x} y={0.5 * size.height - center.y * scale.y} {...handlers}>
         <Layer listening={false}>{cells}</Layer>
         <Layer listening={false} name={DRAFT_LAYER} />
         <Layer listening={false}>
--- src/store/hooks/store-hooks.ts
+++ src/store/hooks/store-hooks.ts
@@ -1,6 +1,20 @@
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState, store } from '../store';
 
 type AppDispatch = typeof store.dispatch;
+
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+
 export const useAppSelector = useSelector.withTypes<RootState>();
+
+export const useStateRef = <T>(selector: (state: RootState) => T) => {
+  const state = useAppSelector(selector);
+  const ref = React.useRef(state);
+
+  React.useEffect(() => {
+    ref.current = state;
+  }, [state]);
+
+  return ref;
+};
