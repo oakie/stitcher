a71c9fa40c750a2bb7a3d0490cc0d31db039579e
Jonas Ekskog <jonas.ekskog@gmail.com>
2024-09-02 18:51:02 +0200

    Fix issue where the previously signed in account is always automatically selected

--- src/components/auth/sign-in.tsx
+++ src/components/auth/sign-in.tsx
@@ -6,6 +6,7 @@ import { Button, Spinner } from 'react-bootstrap';
 import styled from 'styled-components';
 
 const google = new GoogleAuthProvider();
+google.setCustomParameters({ prompt: 'select_account' });
 
 const Container = styled.div`
   width: 100vw;
@@ -21,21 +22,12 @@ const SignIn: FC<SignInProps> = ({ loading }) => {
   return (
     <Container className="d-flex flex-column justify-content-center">
       {!loading && (
-        <Button
-          variant="outline-secondary"
-          className="mx-auto"
-          onClick={() => signin(google)}
-        >
+        <Button variant="outline-secondary" className="mx-auto" onClick={() => signin(google)}>
           Sign in with google <Icon icon={['fab', 'google']} />
         </Button>
       )}
       {loading && (
-        <Spinner
-          animation="border"
-          variant="secondary"
-          className="mx-auto"
-          style={{ width: '20vh', height: '20vh' }}
-        />
+        <Spinner animation="border" variant="secondary" className="mx-auto" style={{ width: '20vh', height: '20vh' }} />
       )}
     </Container>
   );
